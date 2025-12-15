c2b319aa0aba1bac08c6728af58f21dcddcc3d14
StudentSKV101 <yonatan2804@gmail.com>
2025-12-15 15:54:55 +0200

    feat(package.json, darkTheme.ts): theme

---
+++ Frontend/src/components/Theme/darkTheme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ff9800", // orange accent
+    },
+    background: {
+      default: "#0f0f0f",
+      paper: "#161616",
+    },
+    text: {
+      primary: "#ffffff",
+      secondary: "#b0b0b0",
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+});
