31869761ef3b540bd7dbaf5f900e3667beff08a1
shalev-habany <shalev.habany@gmail.com>
2025-12-17 10:46:15 +0200

    chore(many): blanklines standarts

--- Backend/services/routing_service.py
+++ Backend/services/routing_service.py
@@ -7,12 +7,12 @@ from models.schemas import DirectionsRequest, DirectionsResponse
 
 client = openrouteservice.Client(key=settings.ORS_API_KEY)
 
+
 def get_route(req: DirectionsRequest) -> DirectionsResponse:
     coordinates = list(map(lambda x: x.to_lng_lat(), req.waypoints))
 
     geometry = directions(client, coordinates, optimize_waypoints=True)["routes"][0]["geometry"]
     decoded = convert.decode_polyline(geometry)
-    polyline = [[lat, lng] for lng, lat in decoded["coordinates"]] # convert LngLat to LatLng
+    polyline = [[lat, lng] for lng, lat in decoded["coordinates"]]  # convert LngLat to LatLng
 
     return DirectionsResponse(polyline=polyline)
-
