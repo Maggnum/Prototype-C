40fa86942157d1a60609e6391415953424d01fb3
shalev-habany <shalev.habany@gmail.com>
2025-12-23 11:20:15 +0200

    del(useMapMode.ts): uneeded hook

--- Frontend/src/components/PolygonsTable/useMapMode.ts
+++
@@ -1,9 +0,0 @@
-import { MapMode } from "@/models";
-import { useSetAtom } from "jotai";
-import { mapModeAtom } from "@/states";
-
-export const useMapMode = () => {
-  const setMapModeAtom = useSetAtom(mapModeAtom);
-  const setMapMode = (mapMode: MapMode) => setMapModeAtom(mapMode);
-  return setMapMode;
-};
